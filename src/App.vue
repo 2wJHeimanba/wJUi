e364a5d4184e86d7fb45d0f270410b46dc508c90
水平致远 <zwj18506698903@163.com>
2021-09-23 17:47:17 +0800

    carousel0923

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <job-carousel></job-carousel>
     <!-- <img alt="Vue logo" src="./assets/logo.png" @click="carousel_toggle=true"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <!-- <Carousel :data_list="images_list" v-show="carousel_toggle" :toggle.sync="carousel_toggle" /> -->
     <!-- <DatePicker :date="time"/> -->
     <div id="nav">
--- src/libs/51Ui/components/JobCarousel.vue
+++ src/libs/51Ui/components/JobCarousel.vue
@@ -1,11 +1,25 @@
 <template>
-    <div class="job-carousel">
-        <div class="job-carousel-image">
-            <img src="http://starcdn.51job.com/TeachIn/2109/222308_169.jpg" alt="" srcset="">
-        </div>
-        <div class="job-carousel-title">
-            <ul>
-                <li v-for="(item,index) in temporaryArr" :key="index">{{item.title}}</li>
+    <div class="job-carousel" @mouseover="stopShuffling" @mouseleave="startShuffling">
+        <transition-group name="list" enter-active-class="carousel-image-enter" leave-active-class="carousel-image-leave">
+            <div class="job-carousel-image" v-for="(item,index) in temporaryArr" :key="index" v-show="index===active_key">
+                <img :src="item.href">
+            </div>
+        </transition-group>
+        
+        <div class="job-carousel-title" ref="boxScrollTop">
+            <ul ref="ulScrollTop">
+                <li v-for="(item,index) in temporaryArr" :key="index" 
+                    :class="[{'active-item':index===active_key}]"
+                    @mouseover="mouseKeyOver(index)" 
+                    :style="{
+                        height:height+'px'
+                    }"
+                >
+                    <div class="job-carousel-box-title" >
+                        <div>{{item.title}}</div>
+                        <div class="active-dot" ref="dotTransition" :style="{top:dot_distance+'px'}"></div>
+                    </div>
+                </li>
             </ul>
         </div>
     </div>
@@ -14,21 +28,87 @@
 <script>
 export default {
     name:'job-carousel',
+    props:{
+        height:{
+            type:[String,Number],
+            default:40
+        },
+
+    },
     data(){
         return {
             temporaryArr:[
-                {title:'东方本田'},
-                {title:'广汽传祺'},
-                {title:'一起奥迪'},
-                {title:'华晨宝马'},
-                {title:'雷克萨斯'},
-                {title:'凯迪拉克'},
-                {title:'奔驰'},
-                {title:'北京现代'},
-                {title:'福特野马'},
-                {title:'英菲尼迪'}
-            ]
+                {title:'赢家时尚集团',href:"http://starcdn.51job.com/TeachIn/2109/170034_510.jpg"},
+                {title:'广汽传祺',href:"http://starcdn.51job.com/TeachIn/2109/134846_591.jpg"},
+                {title:'飞利浦',href:"http://starcdn.51job.com/TeachIn/2108/165954_658.jpg"},
+                {title:'华晨宝马',href:"http://starcdn.51job.com/TeachIn/2109/142313_777.jpg"},
+                {title:'雷克萨斯',href:"http://starcdn.51job.com/TeachIn/2109/130001_202.jpg"},
+                {title:'凯迪拉克',href:"http://starcdn.51job.com/TeachIn/2108/182016_232.jpg"},
+                {title:'奔驰',href:"http://starcdn.51job.com/TeachIn/2108/165954_658.jpg"},
+                {title:'北京现代',href:"http://starcdn.51job.com/TeachIn/2109/205122_145.jpg"},
+                {title:'福特野马',href:"http://starcdn.51job.com/TeachIn/2109/182546_909.jpg"},
+                {title:'英菲尼迪',href:"http://starcdn.51job.com/TeachIn/2109/134313_984.jpg"}
+            ],
+            active_key:0,
+            dot_distance:0,
         }
+    },
+    watch:{
+        active_key(newVal,oldVal){
+            if(newVal>2){                
+                this.timer_wen=setInterval(function(){test(newVal,oldVal)},10)         
+            }else if(newVal===0){
+                this.$refs.boxScrollTop.scrollTop=0
+            }
+
+            this.dot_distance=newVal*Number(this.height);
+            let distance=(Math.abs(newVal-oldVal)*Number(this.height))/20;
+            let countdown=200;
+            let _this=this;
+
+            function test(nval,oval){
+                countdown=countdown-10;
+                if(nval>oval){
+                    _this.$refs.boxScrollTop.scrollTop=_this.$refs.boxScrollTop.scrollTop+Math.round(distance);
+                }else if(nval<oval){
+                    _this.$refs.boxScrollTop.scrollTop=_this.$refs.boxScrollTop.scrollTop-Math.round(distance);
+                }
+                if(countdown<=0){
+                    clearInterval(_this.timer_wen)
+                }
+            }
+        }
+    },
+    mounted(){
+        this.shuffling();
+    },
+    methods:{
+        mouseKeyOver(i){
+            this.active_key=i;
+        },
+
+
+        shuffling(){
+            this.timer=setInterval(()=>{
+                if(this.active_key<this.temporaryArr.length-1){
+                    this.active_key++
+                }else{
+                    this.active_key=0
+                }
+            },2500)
+        },
+
+
+        stopShuffling(){
+            clearInterval(this.timer);
+            clearInterval(this.timer_wen)
+        },
+        startShuffling(){
+            this.shuffling()
+        }
+    },
+    beforeDestroy(){
+        clearInterval(this.timer)
     }
 }
 </script>
@@ -37,8 +117,6 @@ export default {
 .job-carousel{
     width: 900px;
     height: 210px;
-    border: 2px solid #ccc;
-    /* border-radius: 12px; */
     margin-left: 180px;
     position: relative;
     overflow: hidden;
@@ -52,47 +130,82 @@ export default {
     width: 100%;
     height: 100%;
 }
+.carousel-image-enter{
+    animation: image-enter 0.39s linear;
+}
+.carousel-image-leave{
+    animation: image-enter 0.38s linear reverse;
+}
+@keyframes image-enter {
+    from{opacity: 0;}
+    to{opacity: 1;}
+}
+
 .job-carousel-title{
     position: absolute;
     right: 0;
     bottom: 0;
     top: 0;
     width: 13%;
-    background: rgba(0,0,0,0.25);
+    background: rgba(0,0,0,0.35);
     color: #fff;
     overflow-y: auto;
 }
+.active-dot{
+    position: absolute;
+    top: 0;left: 0;
+    width: 100%;
+    height: 50px;
+    background: rgba(0, 0, 0, 0.06);
+    z-index: -1;
+    transition: all 0.2s ease-in-out;
+    border-left: 2px solid #fff;
+}
+
 .job-carousel-title::-webkit-scrollbar {
-    width : 3px;  
+    width: 0px;
+}
+.job-carousel-title:hover::-webkit-scrollbar {
+    width: 3px;
 }
 .job-carousel-title::-webkit-scrollbar-thumb {
-    /*滚动条里面小方块*/
-    border-radius   : 10px;
-    background-color: #fff;
-    background-image: -webkit-linear-gradient(
-        45deg,
-        rgba(255, 255, 255, 0.2) 25%,
-        transparent 25%,
-        transparent 50%,
-        rgba(255, 255, 255, 0.2) 50%,
-        rgba(255, 255, 255, 0.2) 75%,
-        transparent 75%,
-        transparent
-    );
+    border-radius:10px;
+    background-color: rgb(4, 180, 250);
 }
 .job-carousel-title::-webkit-scrollbar-track {
-    /*滚动条里面轨道*/
-    box-shadow   : inset 0 0 5px rgba(0, 0, 0, 0.2);
-    background   : #ededed;
-    border-radius: 10px;
+    box-shadow:inset 0 0 5px rgba(0, 0, 0, 0.2);
+    background:#ededed;
 }
 
 
 .job-carousel-title>ul{
     list-style: none;
-    /* border: 1px solid red; */
+    position: relative;
+    z-index: 1;
 }
-.job-carousel-title>ul>li{
-    padding: 5px 0;
+.job-carousel-title>ul>li>div{
+    /* padding: 5px 0; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+    cursor: pointer;
+}
+.job-carousel-title>ul>li.active-item{
+    height: 50px !important;
+    /* border: 1px solid #fff; */
+}
+.job-carousel-title>ul>li.active-item>div{
+    /* padding: 8px; */
+    height: 50px;
+    /* border: 2px solid #fff; */
+    font-size: 16px;
+    background-image: linear-gradient(to bottom,rgb(255, 102, 0),rgb(250, 209, 28));
+    background-clip: text;
+    color: transparent;
+    text-shadow: 0 0 2px rgba(255, 255, 255, 0.28);
+}
+.job-carousel-box-title{
+    width: 100%;height: 100%;
 }
 </style>
